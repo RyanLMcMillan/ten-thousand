5b10ef55d489fbeb914b936b02981faf653af5d5
Ryan <superryanman1@gmail.com>
2022-07-25 18:52:52 -0700

    idk im tired ill finish this later

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -15,6 +15,7 @@ class GameLogic:
     @staticmethod
     def calculate_score(tup):
         score = 0
+        list(tup).sort()
         roll = Counter(tup).most_common()
 # 6 of a kind
         if roll[0][1] == 6:
@@ -83,6 +84,39 @@ class GameLogic:
                 return score
 # triple Doubles
         if len(roll) > 2:
+            if roll[0][1] and roll[1][1] and roll[2][1] == 2:
+               score += 1500
+
+# straight
+        if len(roll) == 6:
+            score += 1500
+# full house
+#        if len(roll) >= 2:
+ #           if roll[0][3] and roll[1][2]:
+  #              score += 1500
+   #         elif len(roll) == 3:
+    #            if roll[2][0] == 5:
+     #               score += 50
+      #          elif[2][0] == 1:
+       #             score += 100
+        #    return score
+    #pair 1's
+        if roll[0][0] == 1 and roll[0][1] == 2:
+            score += 200
+    #pair 5's
+        if roll[0][1] == 2 and roll[0][0] == 5:
+            score += 100
+
+        elif len(roll) > 1:
+            if roll[1][1] == 2 and roll[1][0] == 5:
+                score += 100
+    #single 1
+        #elif roll[0][0] == 1:
+         #   score +=  100
+
+    # single 5
+        elif roll[0][0] == 5:
+            score += 50
 
         return score
 
